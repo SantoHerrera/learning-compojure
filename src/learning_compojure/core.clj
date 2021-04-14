3cadb970c2ba88c1352c98608fce1114d51ee36d
Rafael Herrera <santoElias65537@gmail.com>
2021-04-13 18:45:47 -0600

    stuck

--- src/learning_compojure/core.clj
+++ src/learning_compojure/core.clj
@@ -1,34 +1,45 @@
 (ns learning-compojure.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.params :as p]
+            [ring.util.response :as r]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found]]
             [hiccup.core :refer :all]
             [hiccup.form :as form])
   (:gen-class))
 
-
+;https://gist.github.com/zehnpaard/665edf183818b4df707b5f0535ecdc0c
+;zip where template came from. couldnt get it to work, so made new project with a lot of same code.
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+(def users {:email "user1"
+            :password "fuck"})
+
 
 (defn does-user-exist?
-  []
-  ;[req
-  ; (let [{:keys [params]} req
-  ;       email (get params "email")
-  ;       password (get params "password")]
- (html [:div [:p "email" "email"]
-             [:br]
-             [:p "password" "password"]]))
+  [data email password]
+  (and (= email (:email data))
+       (= password (:password data))))
+
 
 
 (defn not-found1 []
     "<h1>404 Error!</h1>
      <b>Page not found!</b>
      <p><a href='..'>Return to main page</p>")
+
+(defn does-exist []
+  (html [:h1 "does fucking exits"]))
+
+(defn doesnt-exist []
+  (html [:h1 "doesnt exist"]))
+
+(defn test1 [boolean]
+  (html [:h1 boolean "nice"]))
+
 
 
 (defn get-login-info [req]
@@ -41,6 +52,15 @@
                [:p "email youve submitted " email]
                [:br]
                [:p "password " password]])))
+
+
+(defn get-login-infoV2 [req]
+    (let [{:keys [params]} req
+          param-name (get params "name")
+          email (get params "email")
+          password (get params "password")
+          user-exists (does-user-exist? users email password)]
+        (html [:h1 (str user-exists email password) " lost"])))
 
 
 (defn login
@@ -65,13 +85,17 @@
        <p>What would you like to do?</p>
        <p><a href='./get-form.html'>Submit a GET request</a></p>
        <p><a href='./post-form.html'>Submit a POST request</a></p>
+       <p><a href='./login'>login</a></p>
    </div>")
 
 
 (defroutes routes
   (GET "/" [req] (main req))
   (GET "/login" [] (login))
-  (POST "/get-login-info" [req] (get-login-info req))
+  (GET "/doesnt-exist" [] (doesnt-exist))
+  (GET "/does-exist" [] (does-exist))
+  ;(GET "/test" (test1))
+  (POST "/get-login-info" [req] (get-login-infoV2 req))
   ;(GET "/does-user-exist?" req (does-user-exist?))
   (not-found (not-found1)))
 
